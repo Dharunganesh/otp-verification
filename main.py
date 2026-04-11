7440343ea631337b400bffdd76b837d30f86443b
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2026-04-11 11:18:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from pydantic import BaseModel
 import psycopg2
 import os
 from dotenv import load_dotenv
-
+from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://vote-pledge.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # 🔹 Database connection (Neon / PostgreSQL)
 conn = psycopg2.connect(os.getenv("DATABASE_URL"))
 
